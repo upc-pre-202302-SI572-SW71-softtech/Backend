f4b095dd4c8df53d0e854dbd9c3706090bb26199
J_Arturo <73813395+pepper419@users.noreply.github.com>
2023-11-03 22:39:30 -0500

    little fix

--- src/main/java/com/softtech/backendapi/routes/api/RoutesController.java
+++ src/main/java/com/softtech/backendapi/routes/api/RoutesController.java
@@ -15,38 +15,5 @@ import java.util.List;
 @RequestMapping("api/travel")
 @AllArgsConstructor
 public class RoutesController {
-    @Autowired
-    private TravelServiceImpl travelCrudService;
-
-    @PostMapping
-    public Travel save(@RequestBody Travel travel){
-        return travelCrudService.save(travel);
-    }
-
-    @GetMapping
-    public List<Travel> getAll() {return travelCrudService.getAll();}
-
-    @GetMapping("{id}")
-    public Travel getById(@PathVariable Integer id) {
-        return travelCrudService.getById(id).get();
-    }
-
-    @PutMapping("{id}")
-    public ResponseEntity<Travel> update (@PathVariable Integer id, @RequestBody Travel travel  ){
-        if (id.equals(travel.getId())){
-            travelCrudService.update(travel);
-            return new ResponseEntity<>(travel, HttpStatus.OK);
-        } else {
-            return ResponseEntity.badRequest().build();
-        }
-    }
-
-    @DeleteMapping("{id}")
-    public ResponseEntity<Travel> delete(@PathVariable Integer id){
-        if (travelCrudService.deleteById(id)){
-            return ResponseEntity.ok().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
+  
 }
