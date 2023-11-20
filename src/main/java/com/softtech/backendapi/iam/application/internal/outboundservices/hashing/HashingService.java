72e70f9eb9cd611f6469649b643c74a6862a7b07
JCayllahuaG <u202019558@upc.edu.pe>
2023-11-20 17:07:26 -0500

    feat: hashing service for password added

---
+++ src/main/java/com/softtech/backendapi/iam/application/internal/outboundservices/hashing/HashingService.java
@@ -0,0 +1,6 @@
+package com.softtech.backendapi.iam.application.internal.outboundservices.hashing;
+
+public interface HashingService {
+    String encode(CharSequence rawPassword);
+    boolean matches(CharSequence rawPassword, String encodedPassword);
+}
