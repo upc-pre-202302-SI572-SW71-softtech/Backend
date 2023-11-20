0b58d1b1fb475f5eb25a278722b6f4900f457ad7
JCayllahuaG <u202019558@upc.edu.pe>
2023-11-20 17:15:50 -0500

    feat: open api config added to document rest api endpoints added

--- src/main/java/com/softtech/backendapi/BackendapiApplication.java
+++ src/main/java/com/softtech/backendapi/BackendapiApplication.java
@@ -2,7 +2,9 @@ package com.softtech.backendapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BackendapiApplication {
 
