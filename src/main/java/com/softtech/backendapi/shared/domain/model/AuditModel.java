9cb9025b5a611eaf43a5ee5714e2053d577f998d
JCayllahuaG <u202019558@upc.edu.pe>
2023-11-20 17:05:02 -0500

    feat: audit model updated

--- src/main/java/com/softtech/backendapi/shared/domain/model/AuditModel.java
+++ src/main/java/com/softtech/backendapi/shared/domain/model/AuditModel.java
@@ -2,29 +2,27 @@ package com.softtech.backendapi.shared.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
-@Getter
-@Setter
-@MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-@JsonIgnoreProperties(
-        value = {"createdAt", "updatedAt"},
-        allowGetters = true
-)
-public abstract class AuditModel {
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "create_at", nullable = false, updatable = false)
+@MappedSuperclass
+@Data
+public class AuditModel {
+
     @CreatedDate
+    @Column(nullable = false, updatable = false)
     private Date createdAt;
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "update_at", nullable = false)
-    @LastModifiedBy
+
+    @LastModifiedDate
+    @Column(nullable = false)
     private Date updatedAt;
+
 }
